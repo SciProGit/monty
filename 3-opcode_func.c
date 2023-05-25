c1cadafd6e0678d8b8c8817063d3d332193f3fc4
SciProGit <122888303+SciProGit@users.noreply.github.com>
2023-05-25 10:01:43 -0700

    opt functions

---
+++ 3-opcode_func.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+}
+/**
+ * stack - adds data to top of stack
+ * @stack: doubly linked list
+ * @line_number: the line
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+glob.s_or_q = 's';
+}
+/**
+ * queue - adds data to top of stack
+ * @stack: doubly linked list
+ * @line_number: the line
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+glob.s_or_q = 'q';
+}
