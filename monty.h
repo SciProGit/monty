da5c3d5f167c05de0d1160fc87bafbb14441e2ad
SciProGit <122888303+SciProGit@users.noreply.github.com>
2023-05-25 09:18:04 -0700

    Header File

--- monty.h
+++ monty.h
@@ -1,5 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -31,4 +37,50 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#endif /* MONTY_H */
+/**
+ * struct global - global variables
+ * @stk: doubly linked list
+ * @get_file: file
+ * @s_or_q: decide stack or queue
+ *
+ * Description:  global variables
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct global
+{
+	stack_t *stk;
+	FILE *get_file;
+	char s_or_q;
+} glob_t;
+
+/*Operation codes*/
+void instruc(char *s, unsigned int line_count);
+int args_check(char *s);
+void clean(stack_t *head);
+void stack_del(stack_t **stack);
+void tb_del(stack_t **stack, int i);
+void top_bot(stack_t **stack, const int i);
+void tb_del(stack_t **stack, int i);
+
+/*opcode functions*/
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divi(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
+extern glob_t glob;
+glob_t glob;
+
+#endif /*  MONTY_H */
