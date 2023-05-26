4d56d557d071efc4e477f766c7b6ca3c64c8415a
SciProGit <122888303+SciProGit@users.noreply.github.com>
2023-05-25 18:16:45 -0700

    monty execution function

--- monty_exn.c
+++ monty_exn.c
@@ -35,7 +35,7 @@ void instruc(char *s, unsigned int line_num)
 	{
 		if (!strcmp(op, ins[i].opcode))
 		{
-			ins[i].f(&(s), line_num);
+			ins[i].f(&(globe.stks), line_num);
 			return;
 		}
 	i++;
